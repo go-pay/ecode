ebfebefeee1e71c1b7e608f98d2743d2a837cf47
Jerry <85411418@qq.com>
2023-11-24 10:09:20 +0800

    v0.0.1

--- ecode.go
+++ ecode.go
@@ -2,58 +2,149 @@ package ecode
 
 import (
 	"errors"
-	"math"
+	"fmt"
 	"strconv"
 
-	"google.golang.org/grpc/codes"
+	"google.golang.org/genproto/googleapis/rpc/errdetails"
 	"google.golang.org/grpc/status"
 )
 
 // Error struct
 type Error struct {
-	code    int    `json:"code"`
-	message string `json:"message"`
-}
-
-// New error code and msg
-func New(code int, msg string) *Error {
-	return &Error{code: code, message: msg}
+	Status
+	cause error
 }
 
 func (e *Error) Error() string {
-	return e.message
+	return fmt.Sprintf("error: code = %d reason = %s message = %s metadata = %v cause = %v", e.Status.Code, e.Status.Reason, e.Status.Message, e.Metadata, e.cause)
 }
 
-// Code return error code
-func (e *Error) Code() int { return e.code }
+// Code returns the code of the error.
+func (e *Error) Code() int { return int(e.Status.Code) }
 
-// Message return error message
-func (e *Error) Message() string {
-	return e.message
+// Message returns the message of the error.
+func (e *Error) Message() string { return e.Status.Message }
+
+// Reason returns the reason of the error.
+func (e *Error) Reason() string { return e.Status.Reason }
+
+// Unwrap provides compatibility for Go 1.13 error chains.
+func (e *Error) Unwrap() error { return e.cause }
+
+// Is matches each error in the chain with the target value.
+func (e *Error) Is(err error) bool {
+	if se := new(Error); errors.As(err, &se) {
+		return se.Status.Code == e.Status.Code && se.Status.Reason == e.Status.Reason
+	}
+	return false
 }
 
+// Equal matches error from code and reason.
+func (e *Error) Equal(code int, reason ...string) bool {
+	se := &Error{Status: Status{
+		Code: int32(code),
+	}}
+	if len(reason) == 1 {
+		se.Status.Reason = reason[0]
+		return se.Status.Code == e.Status.Code && se.Status.Reason == e.Status.Reason
+	}
+	return se.Status.Code == e.Status.Code
+}
+
+// GRPCStatus returns the Status represented by error.
 func (e *Error) GRPCStatus() *status.Status {
-	return status.New(codes.Code(uint32(math.Abs(float64(e.Code())))), e.Error())
+	gs, _ := status.New(DefaultConverter.ToGRPCCode(int(e.Status.Code)), e.Status.Message).
+		WithDetails(&errdetails.ErrorInfo{Reason: e.Status.Reason, Metadata: e.Metadata})
+	return gs
 }
 
-// analyse error info
-func AnalyseError(err error) (e *Error) {
+// WithCause with the underlying cause of the error.
+func (e *Error) WithCause(cause error) *Error {
+	err := DeepClone(e)
+	err.cause = cause
+	return err
+}
+
+// WithMetadata with an MD formed by the mapping of key, value.
+func (e *Error) WithMetadata(md map[string]string) *Error {
+	err := DeepClone(e)
+	err.Metadata = md
+	return err
+}
+
+// ============================================================================================================
+
+// New returns an error object for the code, message.
+func New(code int, reason string, message string) *Error {
+	return &Error{Status: Status{
+		Code:    int32(code),
+		Reason:  reason,
+		Message: message,
+	}}
+}
+
+// DeepClone deep clone error to a new error.
+func DeepClone(err *Error) *Error {
 	if err == nil {
-		return SuccessV1
+		return nil
 	}
-	if errors.As(err, &e) {
-		return e
+	metadata := make(map[string]string, len(err.Metadata))
+	for k, v := range err.Metadata {
+		metadata[k] = v
 	}
-	return errStringToError(err.Error())
+	return &Error{
+		cause: err.cause,
+		Status: Status{
+			Code:     err.Status.Code,
+			Reason:   err.Status.Reason,
+			Message:  err.Status.Message,
+			Metadata: metadata,
+		},
+	}
 }
 
-func errStringToError(e string) *Error {
+// FromError try to convert an error to *Error.
+// It supports wrapped errors.
+func FromError(err error) *Error {
+	if err == nil {
+		return Success
+	}
+	if se := new(Error); errors.As(err, &se) {
+		return se
+	}
+	gs, ok := status.FromError(err)
+	if !ok {
+		return New(UnknownCode, UnknownReason, err.Error())
+	}
+	ret := New(DefaultConverter.FromGRPCCode(gs.Code()), UnknownReason, gs.Message())
+	for _, detail := range gs.Details() {
+		switch d := detail.(type) {
+		case *errdetails.ErrorInfo:
+			ret.Status.Reason = d.Reason
+			return ret.WithMetadata(d.Metadata)
+		}
+	}
+	return ret
+}
+
+// AnalyseError analyse error info
+func AnalyseError(err error) (e2 *Error) {
+	if err == nil {
+		return Success
+	}
+	if errors.As(err, &e2) {
+		return e2
+	}
+	return errStringToErrorV2(err.Error())
+}
+
+func errStringToErrorV2(e string) *Error {
 	if e == "" {
-		return SuccessV1
+		return Success
 	}
 	i, err := strconv.Atoi(e)
 	if err != nil {
-		return New(-1, e)
+		return New(-1, UnknownReason, e)
 	}
-	return New(i, e)
+	return New(i, UnknownReason, e)
 }
--- ecode_test.go
+++ ecode_test.go
@@ -1,33 +1,80 @@
 package ecode
 
 import (
+	"errors"
+	"net/http"
 	"testing"
 
 	"github.com/go-pay/xlog"
 )
 
-func TestEcode(t *testing.T) {
+var (
+	// base error
+	ParameterErr = New(1000400, "request param error", "request param error")
+)
+
+func TestEcodeWithReason(t *testing.T) {
 	xlog.Level = xlog.DebugLevel
-	e := AnalyseError(ForbiddenErr)
-	xlog.Debug(e.Error())
-	xlog.Debug(e.Code())
-	xlog.Debug(e.Message())
+	e := FromError(ParameterErr)
+	xlog.Debug(e.Error())   // error: code = 1000400 reason =  message = equest param error metadata = map[] cause = <nil>
+	xlog.Debug(e.Code())    // 1000400
+	xlog.Debug(e.Message()) // request param error
 	xlog.Info("============================")
 
-	e2 := AnalyseError(UnauthorizedErr)
-	xlog.Debug(e2.Error())
-	xlog.Debug(e2.Code())
-	xlog.Debug(e2.Message())
+	Success = New(1, "SUCCESS", "success")
+
+	e2 := FromError(nil)
+	xlog.Debug(e2.Error())   // error: code = 1 reason = SUCCESS message = success metadata = map[] cause = <nil>
+	xlog.Debug(e2.Code())    // 1
+	xlog.Debug(e2.Reason())  // SUCCESS
+	xlog.Debug(e2.Message()) // success
 	xlog.Info("============================")
 
-	sms := New(10000, "中国电信")
-	xlog.Debug(sms.Error())
-	xlog.Debug(sms.Code())
-	xlog.Debug(sms.Message())
+	sms := New(10000, "CTCC", "中国电信").WithMetadata(map[string]string{
+		"name":   "jerry",
+		"reason": "我是metadata",
+	})
+	xlog.Debug(sms.Error())   // error: code = 10000 reason = CTCC message = 中国电信 metadata = map[name:jerry reason:我是metadata] cause = <nil>
+	xlog.Debug(sms.Code())    // 10000
+	xlog.Debug(sms.Reason())  // CTCC
+	xlog.Debug(sms.Message()) // 中国电信
+	xlog.Debug(sms.Metadata)  // map[name:jerry reason:我是metadata]
 	xlog.Info("============================")
 
-	mms := New(10086, "中国移动")
-	xlog.Debug(mms.Error())
-	xlog.Debug(mms.Code())
-	xlog.Debug(mms.Message())
+	mms := New(10086, "CMCC", "中国移动").WithCause(errors.New("我是原因"))
+	xlog.Debug(mms.Error())   // error: code = 10086 reason = CMCC message = 中国移动 metadata = map[] cause = 我是原因
+	xlog.Debug(mms.Code())    // 10086
+	xlog.Debug(mms.Reason())  // CMCC
+	xlog.Debug(mms.Message()) // 中国电信
+	xlog.Debug(mms.Unwrap())  // 我是原因
+}
+
+func TestIs(t *testing.T) {
+	tests := []struct {
+		name string
+		e    *Error
+		err  error
+		want bool
+	}{
+		{
+			name: "true",
+			e:    New(404, "test", ""),
+			err:  New(http.StatusNotFound, "test", ""),
+			want: true,
+		},
+		{
+			name: "false",
+			e:    New(0, "test", ""),
+			err:  errors.New("test"),
+			want: false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if ok := tt.e.Is(tt.err); ok != tt.want {
+				t.Errorf("Error.Error() = %v, want %v", ok, tt.want)
+			}
+		})
+	}
 }
