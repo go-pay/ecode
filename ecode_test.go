f195cf945afe436096d043b9a097282ce166bbff
Jerry <85411418@qq.com>
2023-11-24 11:05:01 +0800

    v0.0.2

--- ecode_test.go
+++ ecode_test.go
@@ -2,10 +2,9 @@ package ecode
 
 import (
 	"errors"
+	"log"
 	"net/http"
 	"testing"
-
-	"github.com/go-pay/xlog"
 )
 
 var (
@@ -14,39 +13,38 @@ var (
 )
 
 func TestEcodeWithReason(t *testing.T) {
-	xlog.Level = xlog.DebugLevel
 	e := FromError(ParameterErr)
-	xlog.Debug(e.Error())   // error: code = 1000400 reason =  message = equest param error metadata = map[] cause = <nil>
-	xlog.Debug(e.Code())    // 1000400
-	xlog.Debug(e.Message()) // request param error
-	xlog.Info("============================")
+	log.Println(e.Error())   // error: code = 1000400 reason =  message = equest param error metadata = map[] cause = <nil>
+	log.Println(e.Code())    // 1000400
+	log.Println(e.Message()) // request param error
+	log.Println("============================")
 
 	Success = New(1, "SUCCESS", "success")
 
 	e2 := FromError(nil)
-	xlog.Debug(e2.Error())   // error: code = 1 reason = SUCCESS message = success metadata = map[] cause = <nil>
-	xlog.Debug(e2.Code())    // 1
-	xlog.Debug(e2.Reason())  // SUCCESS
-	xlog.Debug(e2.Message()) // success
-	xlog.Info("============================")
+	log.Println(e2.Error())   // error: code = 1 reason = SUCCESS message = success metadata = map[] cause = <nil>
+	log.Println(e2.Code())    // 1
+	log.Println(e2.Reason())  // SUCCESS
+	log.Println(e2.Message()) // success
+	log.Println("============================")
 
 	sms := New(10000, "CTCC", "中国电信").WithMetadata(map[string]string{
 		"name":   "jerry",
 		"reason": "我是metadata",
 	})
-	xlog.Debug(sms.Error())   // error: code = 10000 reason = CTCC message = 中国电信 metadata = map[name:jerry reason:我是metadata] cause = <nil>
-	xlog.Debug(sms.Code())    // 10000
-	xlog.Debug(sms.Reason())  // CTCC
-	xlog.Debug(sms.Message()) // 中国电信
-	xlog.Debug(sms.Metadata)  // map[name:jerry reason:我是metadata]
-	xlog.Info("============================")
+	log.Println(sms.Error())   // error: code = 10000 reason = CTCC message = 中国电信 metadata = map[name:jerry reason:我是metadata] cause = <nil>
+	log.Println(sms.Code())    // 10000
+	log.Println(sms.Reason())  // CTCC
+	log.Println(sms.Message()) // 中国电信
+	log.Println(sms.Metadata)  // map[name:jerry reason:我是metadata]
+	log.Println("============================")
 
 	mms := New(10086, "CMCC", "中国移动").WithCause(errors.New("我是原因"))
-	xlog.Debug(mms.Error())   // error: code = 10086 reason = CMCC message = 中国移动 metadata = map[] cause = 我是原因
-	xlog.Debug(mms.Code())    // 10086
-	xlog.Debug(mms.Reason())  // CMCC
-	xlog.Debug(mms.Message()) // 中国电信
-	xlog.Debug(mms.Unwrap())  // 我是原因
+	log.Println(mms.Error())   // error: code = 10086 reason = CMCC message = 中国移动 metadata = map[] cause = 我是原因
+	log.Println(mms.Code())    // 10086
+	log.Println(mms.Reason())  // CMCC
+	log.Println(mms.Message()) // 中国电信
+	log.Println(mms.Unwrap())  // 我是原因
 }
 
 func TestIs(t *testing.T) {
